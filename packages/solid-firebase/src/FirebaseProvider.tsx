ff11c62ffef6cd436a2055a7f9e3ba505b6ebb35
Gabriel Vian <gvian07@gmail.com>
2022-05-16 17:30:32 +0000

    FIx type

--- packages/solid-firebase/src/FirebaseProvider.tsx
+++ packages/solid-firebase/src/FirebaseProvider.tsx
@@ -1,13 +1,13 @@
 import type { FirebaseApp, FirebaseOptions } from 'firebase/app'
 import { initializeApp } from 'firebase/app'
-import type { Component } from 'solid-js'
+import type { Component, JSX } from 'solid-js'
 import { createContext } from 'solid-js'
 
 export const FirebaseContext = createContext<FirebaseApp>()
 
 interface Props {
   config: FirebaseOptions
-  children: Element
+  children: JSX.Element
 }
 
 export const FirebaseProvider: Component<Props> = (props) => {
